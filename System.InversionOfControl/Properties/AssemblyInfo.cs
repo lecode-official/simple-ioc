6d5ecfea429ccbd6aff697d1e531aea85cb2bc4a
David Neumann <david.neumann@live.com>
2016-02-03 21:20:27 +0100

    Bumped up the version number of the project and made it ready to be released on NuGet.

--- System.InversionOfControl/Properties/AssemblyInfo.cs
+++ System.InversionOfControl/Properties/AssemblyInfo.cs
@@ -22,7 +22,7 @@ using System.Resources;
 
 #region Assembly Version Information
 
-[assembly: AssemblyVersion("0.1.0.*")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyVersion("0.1.1.*")]
+[assembly: AssemblyFileVersion("0.1.1.0")]
 
 #endregion
