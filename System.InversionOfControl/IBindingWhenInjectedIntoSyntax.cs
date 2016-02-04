8c6916fb3a7dcd76cf40e1f1de7f529660b86180
David Neumann <david.neumann@live.com>
2016-02-04 14:19:59 +0100

    Made it possible to directly specify the scope and the type that is injected into.

--- System.InversionOfControl/IBindingWhenInjectedIntoSyntax.cs
+++ System.InversionOfControl/IBindingWhenInjectedIntoSyntax.cs
@@ -15,10 +15,22 @@ namespace System.InversionOfControl
         void WhenInjectedInto<T>();
 
         /// <summary>
+        /// Determines that the binding should only be used when the binding is being injected into the specified type or a sub-class of it.
+        /// </summary>
+        /// <param name="type">The type the binding should only be injected into.</param>
+        void WhenInjectedInto(Type type);
+
+        /// <summary>
         /// Determines that the binding should only be used when the binding is being injected exactly into the specified.
         /// </summary>
         /// <typeparam name="T">The type the binding should only be injected into.</typeparam>
         void WhenInjectedExactlyInto<T>();
+
+        /// <summary>
+        /// Determines that the binding should only be used when the binding is being injected exactly into the specified.
+        /// </summary>
+        /// <param name="type">The type the binding should only be injected into.</param>
+        void WhenInjectedExactlyInto(Type type);
 
         #endregion
     }
