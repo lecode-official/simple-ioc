bd639dfcf28f5252223ba54d76e433cc117bbfb5
David Neumann <david.neumann@lecode.de>
2016-02-04 13:57:42 +0100

    Removed some generic type constraints that did not make any sense.

--- System.InversionOfControl/IBindingWhenInjectedIntoSyntax.cs
+++ System.InversionOfControl/IBindingWhenInjectedIntoSyntax.cs
@@ -12,13 +12,13 @@ namespace System.InversionOfControl
         /// Determines that the binding should only be used when the binding is being injected into the specified type or a sub-class of it.
         /// </summary>
         /// <typeparam name="T">The type the binding should only be injected into.</typeparam>
-        void WhenInjectedInto<T>() where T : class;
+        void WhenInjectedInto<T>();
 
         /// <summary>
         /// Determines that the binding should only be used when the binding is being injected exactly into the specified.
         /// </summary>
         /// <typeparam name="T">The type the binding should only be injected into.</typeparam>
-        void WhenInjectedExactlyInto<T>() where T : class;
+        void WhenInjectedExactlyInto<T>();
 
         #endregion
     }
